58f11adbae431ca5acb668c1b4dffd603f52259c
Celeste Thomas <celestesmthomas@gmail.com>
2019-09-14 02:52:31 +0000

    Automatically backed up by Learn

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe "mario" do
-  phrase = "It's-a me, Mario!"
   it "puts out 'It's-a me, Mario!'" do
+    phrase = "It's-a me, Mario!"
     expect{mario}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
